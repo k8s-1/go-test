bd99b01058a38062f56ff847bed71155cdebcd64
pman <pman-$-432894328923@gmail.com>
2024-10-23 20:01:10 +0200

    chore: update file(s): test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 go test ./... -cover -coverpkg -v
+
+# cover shows coverage %
+# coverpkg includes all packages, including those without _test.go files
+# v includes verbose output, showing details about every test
