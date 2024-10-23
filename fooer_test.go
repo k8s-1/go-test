f4d9bc3ce009da7d42d949c27c542f2eda4690ef
pman <pman-$-432894328923@gmail.com>
2024-10-23 19:49:00 +0200

    ...

--- fooer_test.go
+++ fooer_test.go
@@ -1,9 +1,36 @@
 package main
+
 import "testing"
 
+// SIMPLE -> leads to a lot of duplicate testing code
 func TestFooer(t *testing.T) { // Test[NameOfFunction]
-    result := Fooer(3)
-    if result != "Foo" {
-    t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
-    }
+	result := Fooer(3)
+	if result != "Foo" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+	}
+}
+
+// TABLE DRIVEN -> test many values
+func TestFooerTableDriven(t *testing.T) {
+	// Defining the columns of the table
+	var tests = []struct {
+		name  string
+		input int
+		want  string
+	}{
+		// the table itself
+		{"9 should be Foo", 9, "Foo"},
+		{"3 should be Foo", 3, "Foo"},
+		{"1 is not Foo", 1, "1"},
+		{"0 should be Foo", 0, "Foo"},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ans := Fooer(tt.input)
+			if ans != tt.want {
+				t.Errorf("got %s, want %s", ans, tt.want)
+			}
+		})
+	}
 }
