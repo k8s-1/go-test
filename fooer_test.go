752cc541a46a2ae192ffb1b61983f65f87d5ef5e
pman <pman-$-432894328923@gmail.com>
2024-10-23 19:43:11 +0200

    ...

---
+++ fooer_test.go
@@ -0,0 +1,9 @@
+package main
+import "testing"
+
+func TestFooer(t *testing.T) { // Test[NameOfFunction]
+    result := Fooer(3)
+    if result != "Foo" {
+    t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+    }
+}
