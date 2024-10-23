6a08d3fd3aa9025d7cc0b145d317d312413198db
pman <pman-$-432894328923@gmail.com>
2024-10-23 20:07:42 +0200

    ...

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-go test ./... -cover -coverpkg -v
+
+go test ./... -cover -v
+
+# test all packages, otherwise those without _test.go will be ignored completely,
+# leading to higher (misleading) %coverage
+# go test ./... -cover -coverpkg -v
 
 # cover shows coverage %
 # coverpkg includes all packages, including those without _test.go files
 # v includes verbose output, showing details about every test
+
+# run benchmarks, bench is regex to match which benchmarks should be run
+go test -bench=.
